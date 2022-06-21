baec805a0f8940944f464f83d4a8ec0da82253dc
Alex AL <a.alchakov@gmail.com>
2022-06-21 07:52:01 +0300

    Lesson2 - Homework

--- lesson2_HM.py
+++ lesson2_HM.py
@@ -4,83 +4,83 @@
 
 transactions_homework = [
     {
-        "add":True,
-        "amount":69,
-        "products":[
+        "add": True,
+        "amount": 69,
+        "products": [
             "Хлеб",
             "Масло",
             "Сок",
         ]
     },
     {
-        "add":True,
-        "amount":30,
-        "products":[
+        "add": True,
+        "amount": 30,
+        "products": [
             "Сок",
         ]
     },
     {
-        "add":True,
-        "amount":94,
-        "products":[
+        "add": True,
+        "amount": 94,
+        "products": [
             "Мука",
             "Хлеб",
             "Масло",
         ]
     },
     {
-        "add":False,
-        "amount":24,
-        "products":[
+        "add": False,
+        "amount": 24,
+        "products": [
             "Конфеты",
             "Хлеб",
         ]
     },
     {
-        "add":True,
-        "amount":9,
-        "products":[
+        "add": True,
+        "amount": 9,
+        "products": [
             "Хлеб",
             "Шоколад",
             "Гречка",
         ]
     },
     {
-        "add":True,
-        "amount":91,
-        "products":[
+        "add": True,
+        "amount": 91,
+        "products": [
             "Апельсин",
             "Гречка",
         ]
     },
     {
-        "add":True,
-        "amount":84,
-        "products":[
+        "add": True,
+        "amount": 84,
+        "products": [
             "Помидоры",
             "Сок",
         ]
     },
     {
-        "add":False,
-        "amount":45,
-        "products":[
+        "add": False,
+        "amount": 45,
+        "products": [
             "Хлеб",
             "Гречка",
         ]
     },
     {
-        "add":True,
-        "amount":9,
-        "products":[
+        "add": True,
+        "amount": 9,
+        "products": [
             "Лимон",
             "Сок",
         ]
     },
 ]
 
-sum = 0 
-for x in transactions_homework: 
-     if x['add'] and 'Хлеб' in x['products']: 
-             sum += x['amount']
-print (sum)
+sum = 0
+for x in transactions_homework:
+    if x['add'] and 'Хлеб' in x['products']:
+        sum += x['amount']
+print(sum)
