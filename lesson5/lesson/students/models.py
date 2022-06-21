1014b2aa6ff4510f0872bc7deee4fc5cfd7c5ce3
Alex AL <a.alchakov@gmail.com>
2022-06-21 17:26:46 +0300

    models

--- lesson5/lesson/students/models.py
+++ lesson5/lesson/students/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class StudentGroup(models.Model):
+
     name = models.CharField(verbose_name='Группа',
                             max_length=100, null=True, blank=True)
     sudents_number = models.IntegerField(
@@ -17,17 +18,26 @@ class StudentGroup(models.Model):
 
 
 class Student(models.Model):
+
+    SURNAME_CHOICES = {
+        ('Petrenko', 'Петренко'),
+        ('Ivanenko', 'Іваненко'),
+    }
+
     name = models.CharField(verbose_name='Имя', max_length=100)
+    surname = models.CharField(
+        verbose_name='Фамилия', max_length=50, null=True, blank=True, choices=SURNAME_CHOICES)
     birthdate = models.DateField(
         verbose_name='День рождения', null=True, blank=True)
     group = models.ForeignKey(StudentGroup, verbose_name='Группа',
                               on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
 
-def __str__(self):
-    return self.name
+        def save(self, *args, **kwargs):
+            super().save(*args, **kwargs)
 
-
-class Meta:
-    verbose_name = "Студент"
-    verbose_name_plural = "Студенты"
+        class Meta:
+            verbose_name = "Студент"
+            verbose_name_plural = "Студенты"
