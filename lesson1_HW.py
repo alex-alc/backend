3108645249b1a1cf8a531a3679970b8962df38f8
Alex AL <a.alchakov@gmail.com>
2022-06-21 07:50:46 +0300

    Lesson1 - Homework

--- lesson1_HW.py
+++ lesson1_HW.py
@@ -1,20 +1,20 @@
-#Lesson1 - homework
+#Lesson1 - Homework
 
 dict_homework = {
-    
-    "key1":{
-        "d":44,
-        "f":None,
-        "s":{
-            8:44,
-            9:None,
-            10:{"mm":["s", "GET ME", 7]},
+
+    "key1": {
+        "d": 44,
+        "f": None,
+        "s": {
+            8: 44,
+            9: None,
+            10: {"mm": ["s", "GET ME", 7]},
         },
     },
-    
-    "key2":{
-        "fff1":44,
-        "f":None,
+
+    "key2": {
+        "fff1": 44,
+        "f": None,
     },
 }
 
